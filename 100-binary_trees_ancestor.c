ecac70c1f882c95b1b150e82ba5215e814690645
danimohli <danimohlife@gmail.com>
2024-06-05 15:23:07 +0100

    header_added

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -6,15 +6,18 @@
  *
  * Return: Common ancestor node, or NULL if no common ancestor was found
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
+	const binary_tree_t *a, *b;
+
 	if (first == NULL || second == NULL)
 		return (NULL);
 
 	if (first == second)
 		return ((binary_tree_t *)first);
 
-	const binary_tree_t *a = first, b = second;
+	a = first, b = second;
 
 	while (a != b)
 	{
