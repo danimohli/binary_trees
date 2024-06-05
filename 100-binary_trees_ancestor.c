1a1b5c48436995e7dfb56365bdcb8d3ccd1fe723
danimohli <danimohlife@gmail.com>
2024-06-05 23:23:51 +0100

    rewriten for detached case

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,24 +1,57 @@
 #include "binary_trees.h"
 /**
- * binary_trees_ancestor - Finds lowest common ancestor of two nodes
- * @first: The first node
- * @second: The second node
- *
- * Return: Common ancestor node, or NULL if no common ancestor was found
+ * binary_tree_depth - Takes the depth of a node
+ * @tree: Node to measure the depth
+ * Return: depth of the node
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	while (tree && tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+	return (depth);
+}
+
+/**
+ * binary_trees_ancestor - get D lowest common ancestor of 2 nodes
+ * @first: point to first node
+ * @second: pointer to second node
+ * Return: lowest common ancestor node of two given nodes
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-		const binary_tree_t *second)
+                                     const binary_tree_t *second)
 {
-	const binary_tree_t *a = first, *b = second;
+	size_t depth_first, depth_second;
 
-	if (first == NULL || second == NULL)
+	if (!first || !second)
 		return (NULL);
 
-	/* Move up the tree until we find a common ancestor */
-	while (a != b)
+	depth_first = binary_tree_depth(first);
+	depth_second = binary_tree_depth(second);
+
+	/* Bring both nodes to the same level */
+	while (depth_first > depth_second)
 	{
-		a = a->parent == NULL ? second : a->parent;
-		b = b->parent == NULL ? first : b->parent;
+		first = first->parent;
+		depth_first--;
 	}
-	return ((binary_tree_t *)a);
+	while (depth_second > depth_first)
+	{
+		second = second->parent;
+		depth_second--;
+	}
+
+	/* Traverse upwards until a common ancestor is found */
+	while (first && second)
+	{
+		if (first == second)
+			return ((binary_tree_t *)first);
+		first = first->parent;
+		second = second->parent;
+	}
+	return (NULL);
 }
